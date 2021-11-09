5330e1e1abe96291e3806f2f77890216ebe7f022
jc5492 <90804001+jc5492@users.noreply.github.com>
2021-11-09 15:11:01 -0500

    Update test_cipher_jc5492.py

--- tests/test_cipher_jc5492.py
+++ tests/test_cipher_jc5492.py
@@ -1,1 +1,29 @@
 from cipher_jc5492 import cipher_jc5492
+import pytest
+
+##part a
+def test_word():
+    example = "test"
+    actual = cipher(example, 1)
+    expected = "uftu"
+    assert actual == expected
+
+##part b
+def test_negative_shift():
+    example = "test"
+    actual = cipher(example, -1)
+    expected = "sdrs"
+    assert actual == expected
+    
+##part c
+def test_symbols():
+    example = "@lphabet"
+    actual = cipher(example,1)
+    expected = "@mqibcfu"
+    assert actual == expected
+    
+##part d
+def test_shift_string():
+    example = "test"
+    with pytest.raises(AssertionError):
+        cipher(example, "one")
